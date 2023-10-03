562df111dcc19e79a8c7c06ecaba173d71e9130a
Chun-gu <chungu.yi92@gmail.com>
2023-10-03 18:03:12 +0900

    feat: add NavBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,35 @@
+import { NavLink } from 'react-router-dom';
+import { styled } from 'styled-components';
+
+export default function NavBar() {
+  return (
+    <nav>
+      <PathList>
+        <li>
+          <NavLink to="/">
+            {({ isActive }) => (
+              <PathName isActive={isActive}>세뱃돈 계산기</PathName>
+            )}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/deposit">
+            {({ isActive }) => (
+              <PathName isActive={isActive}>떼인 돈 계산기</PathName>
+            )}
+          </NavLink>
+        </li>
+      </PathList>
+    </nav>
+  );
+}
+
+const PathList = styled.ul`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const PathName = styled.span<{ isActive: boolean }>`
+  color: ${({ isActive, theme }) =>
+    isActive ? theme.color.red10 : theme.color.slate12};
+`;
