aa287d03c8096d2665ee096ecae42821c1c2ce2a
Chun-gu <chungu.yi92@gmail.com>
2023-10-03 21:04:20 +0900

    feat: barrel export components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
+export { default as InputDisplay } from './InputDisplay';
+export { default as Keypad } from './Keypad';
 export { default as Layout } from './Layout';
+export { default as ResultDisplay } from './ResultDisplay';
