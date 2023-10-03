f7ad8d736e2984a23ab558204c1e8e47af023fb2
Chun-gu <chungu.yi92@gmail.com>
2023-10-03 21:04:37 +0900

    style: add draft of HandselCalculator page

--- src/pages/HandselCalculator.tsx
+++ src/pages/HandselCalculator.tsx
@@ -1,3 +1,46 @@
+import { styled } from 'styled-components';
+
+import { InputDisplay, Keypad, ResultDisplay } from '@/components';
+
 export default function HandselCalculator() {
-  return <h1>세뱃돈 계산기</h1>;
+  return (
+    <Container>
+      <InputDisplay />
+      <SubContainer>
+        <ResultDisplay />
+        <Keypad />
+      </SubContainer>
+    </Container>
+  );
 }
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  gap: 20px;
+  & > div {
+    width: 50%;
+  }
+
+  ${({ theme }) => theme.media.mobile`
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    & > div {
+      width: 100%;
+      max-width: 280px;
+    }
+  `}
+`;
+
+const SubContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  ${({ theme }) => theme.media.mobile`
+  `}
+`;
