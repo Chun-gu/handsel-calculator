b7c97d0f520b48609517b71acb1c16204433f577
Chun-gu <chungu.yi92@gmail.com>
2023-10-03 17:46:33 +0900

    type: specify types for `css`

--- src/styles/theme/media.ts
+++ src/styles/theme/media.ts
@@ -1,6 +1,7 @@
 import { css } from 'styled-components';
 
-import type { RuleSet } from 'styled-components';
+import type { Interpolation, RuleSet } from 'styled-components';
+import type { Styles } from 'styled-components/dist/types';
 
 const size = {
   mobile: [0, 767],
@@ -12,7 +13,8 @@ type Device = keyof typeof size;
 
 export type Media = {
   [key in Device]: (
-    ...args: (string | TemplateStringsArray)[]
+    styles: Styles<object>,
+    ...interpolations: Interpolation<object>[]
   ) => RuleSet<object>;
 };
 
@@ -20,9 +22,9 @@ const devices = Object.keys(size) as Device[];
 
 const media = devices.reduce((obj, device) => {
   // prettier-ignore
-  obj[device] = (...args) => css`
+  obj[device] = (styles, ...interpolations) => css`
     @media screen and (min-width: ${size[device][0]}px) and (max-width: ${size[device][1]}px) {
-        ${args}
+        ${css(styles, ...interpolations)}
       }
   `
 
