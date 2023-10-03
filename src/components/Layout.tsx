f7342c78a3202781e2a7008d8613abccb78d90dd
Chun-gu <chungu.yi92@gmail.com>
2023-10-03 18:02:44 +0900

    style: add global Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,36 @@
 import { Outlet } from 'react-router-dom';
+import { styled } from 'styled-components';
+
+import NavBar from './NavBar';
 
 export default function Layout() {
   return (
-    <>
-      <header>헤더</header>
-      <main>
+    <Container>
+      <Header>
+        <NavBar />
+      </Header>
+      <Main>
         <Outlet />
-      </main>
-    </>
+      </Main>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 320px;
+  max-width: 640px;
+  height: 100vh;
+  padding: 10px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  padding: 10px;
+`;
+
+const Main = styled.main`
+  flex-grow: 1;
+  padding: 10px;
+`;
